4eb19236b0f9aaf03546498e96894236bc914164
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-01-24 20:02:40 +0700

    1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -4,7 +4,27 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+
+	for(int i=1;i<N;i++){
+		T x = d[i];
+		int j = i-1;
+
+		while (j>=0 && d[j] < x)
+		{
+			d[j+1] = d[j];
+			j--;
+		}
+		d[j+1]=x;
+		
+		
+		
+		cout << "Pass "<< i+1 <<":";
+	for(int i = 0; i < N; i++) cout << d[i] << " ";
+	cout << endl;
+	}
 }
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
